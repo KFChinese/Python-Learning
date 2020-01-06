8adb30f9fcead2978243e4d0082e6a22dd67cf07
kfchinese <larry.chiem@getcruise.com>
2020-01-05 21:08:06 -0800

    Changes to modules

--- Basic Tasks/modules.py
+++ Basic Tasks/modules.py
@@ -4,7 +4,8 @@ die = float(input("Roll a die: "))
 print("You rolled a:")
 print(useful_tools.roll_dice(die))
 
-# Modules that use
+# Modules that use external files.
 
 # Google list of python modules
 # https://docs.python.org/3/py-modindex.html
+
