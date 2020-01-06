7cc673d945012944e218074197f71984a5919475
kfchinese <larry.chiem@getcruise.com>
2020-01-05 21:07:39 -0800

    init commit

---
+++ Basic Tasks/modules.py
@@ -0,0 +1,10 @@
+import useful_tools
+# imports useful_tools.py
+die = float(input("Roll a die: "))
+print("You rolled a:")
+print(useful_tools.roll_dice(die))
+
+# Modules that use
+
+# Google list of python modules
+# https://docs.python.org/3/py-modindex.html
