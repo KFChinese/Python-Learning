5570ca1aec7f104fb98cc69878578fa0c720c6bd
kfchinese <larry.chiem@getcruise.com>
2020-01-05 22:43:35 -0800

    Modify to suppliment Classes.

--- Basic Tasks/Classes_and_Objects.py
+++ Basic Tasks/Classes_and_Objects.py
@@ -6,5 +6,10 @@ class Student:
         self.gpa = gpa
         self.is_on_probation = is_on_probation
 
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
 # The class of a student is what makes up a student
 # the object is the student in itself.
--- Basic Tasks/student.py
+++ Basic Tasks/student.py
@@ -2,8 +2,11 @@ from Classes_and_Objects import Student
 
 
 student1 = Student("John", "Business", 3.9, False)
-
+student2 = Student("Mary", "Business", 3.2, True)
 print(student1.name)
 
-#Note: Syntax (Context) is key.
+print(student2.on_honor_roll())
+# Checks if student 2 is on honor roll
 
+
+# Note: Syntax (Context) is key.
