5bb381acb41425077ea94fb62d9363ad56324974
kfchinese <larry.chiem@getcruise.com>
2020-01-05 21:46:14 -0800

    init commit

---
+++ Basic Tasks/Classes_and_Objects.py
@@ -0,0 +1,10 @@
+class Student:
+
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
+
+# The class of a student is what makes up a student
+# the object is the student in itself.
---
+++ Basic Tasks/student.py
@@ -0,0 +1,9 @@
+from Classes_and_Objects import Student
+
+
+student1 = Student("John", "Business", 3.9, False)
+
+print(student1.name)
+
+#Note: Syntax (Context) is key.
+
