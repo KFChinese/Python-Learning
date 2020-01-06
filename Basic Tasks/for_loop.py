63345208ade3a6ffaa9b5a7170c68a782eacb245
kfchinese <larry.chiem@getcruise.com>
2020-01-05 19:16:55 -0800

    First init

---
+++ Basic Tasks/for_loop.py
@@ -0,0 +1,35 @@
+
+for letter in "KFChinese pro":
+    print(letter)
+# Prints each letter in loop
+
+print("")  # Seperator
+
+# Make Array
+fam = ["Mum", "Dad", "Me"]
+
+for fam in fam:
+    print(fam)
+# Prints out array
+
+print("")  # Seperator
+
+for index in range(3, 10):
+    print(index)
+
+#For function prints out range between 3-9
+
+print("")  # Seperator
+
+print(len(fam)) #len == length of array
+
+print("")  # Seperator
+
+for index in range(5):
+
+    if index == 0:
+        print("first Iteration")
+    else:
+        print("Not First")
+
+
